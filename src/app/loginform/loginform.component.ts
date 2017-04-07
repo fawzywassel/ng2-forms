a58907872dd19acd560992ea727a878e53d6e6e4
Unknown <fawzywassel@gmail.com>
2017-04-07 07:41:35 +0200

    step 2

--- src/app/loginform/loginform.component.ts
+++ src/app/loginform/loginform.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+export class User {
+  username:string
+  password:string
+}
+
 @Component({
   selector: 'app-loginform',
   templateUrl: './loginform.component.html',
   styleUrls: ['./loginform.component.css']
 })
+
+
 export class LoginformComponent implements OnInit {
-  username:string 
-  password: string
+  
+  user:User
   constructor() { }
 
   ngOnInit() {
+    this.user ={
+      username:'',
+      password:''
+    }
   }
 
   get loginJson(){
-    return JSON.stringify({
-      username : this.username,
-      password:this.password
-    })
+    return JSON.stringify(
+     this.user
+    )
   }
 
 }
